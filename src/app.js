7f75f0f0241320d83c6c72f998b4f9181cf06011
Jaqueline Téllez <78996295+jaquenwn@users.noreply.github.com>
2025-07-04 21:17:32 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -4,8 +4,21 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+  
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'A stranger'];
+  let action = ['ate', 'peed', 'crushed', 'broke', 'stole'];
+  let what = ['my homework', 'my phone', 'the car', 'my food', 'the fridge'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  function num() {
+		let randomNumber = Math.floor((Math.random() * 5 ) + 1);
+		return randomNumber
+	}
+
+  let text = who[num()] + ' '+ action[num()] + ' '+ what[num()] + ' '+ when[num()];
+  const excusa = excuse.innerHTML;
+  excuse.innerHTML= text;
+}
+
